d218340c3a2e0109b8f10c95981deb8646a899bb
Sageevan Pathmaseelan <pathmasagee@gmail.com>
2023-08-03 22:25:22 +1200

    Add project files.

---
+++ Models/MvponboardingReactContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVPStudioReactOnboarding.Models;
+
+public partial class MvponboardingReactContext : DbContext
+{
+    public MvponboardingReactContext()
+    {
+    }
+
+    public MvponboardingReactContext(DbContextOptions<MvponboardingReactContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Sale> Sales { get; set; }
+
+    public virtual DbSet<Store> Stores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=GGPC\\SQLEXPRESS;Database=MVPOnboardingReact;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.Address).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        modelBuilder.Entity<Sale>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.DateSold).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Sales)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK_Sales_Customer");
+
+            entity.HasOne(d => d.IdNavigation).WithOne(p => p.Sale)
+                .HasForeignKey<Sale>(d => d.Id)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Sales_Product");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Sales)
+                .HasForeignKey(d => d.StoreId)
+                .HasConstraintName("FK_Sales_Store");
+        });
+
+        modelBuilder.Entity<Store>(entity =>
+        {
+            entity.ToTable("Store");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Address).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
